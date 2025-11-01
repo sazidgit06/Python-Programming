d5e01294fe01be0de7c7950f659275ed6387325f
sazid <sazidtec06@gmail.com>
2025-11-01 20:09:20 +0600

    class6 added

---
+++ class6/main.py
@@ -0,0 +1,27 @@
+a = "Owasifa Akter Rifa"
+b = 123
+c = 12.3
+d = None
+e = True
+f = False
+g = complex(8, 2)
+print("Type of a is", type(a))
+print("Type of b is", type(b))
+print("Type of c is", type(c))
+print("Type of d is", type(d))
+print("Type of e is", type(e))
+print("Type of f is", type(f))
+print("Type of g is", g, type(g))
+
+
+
+
+# lsit and tuples
+
+list1 = [2, 4.3, [5, 6], ["Apple", "Bannana"]]
+tuple1 = (4, 5.5, [7, 8], ["A", "B"], ("a", "b"))
+
+list1[0] = 45   # can be modified
+
+print(list1)
+print(tuple1)   # can not be modified
---
+++ class6/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+a = "Owasifa Akter Rifa"
+# b = 123
+# c = 12.3
+# d = None
+# e = True
+# f = False
+# g = complex(8, 2)
+# print("Type of a is", type(a))
+# print("Type of b is", type(b))
+# print("Type of c is", type(c))
+# print("Type of d is", type(d))
+# print("Type of e is", type(e))
+# print("Type of f is", type(f))
+# print("Type of g is", g, type(g))
